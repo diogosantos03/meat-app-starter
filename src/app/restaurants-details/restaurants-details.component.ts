fe5f33e2b5f98a534ee5d7a9b788dc67db068429
Diogo Santos <diogo.santos@dce.ufpb.br>
2018-04-29 20:19:03 -0300

    Add serviço http aos detalhes de restaurantes

--- src/app/restaurants-details/restaurants-details.component.ts
+++ src/app/restaurants-details/restaurants-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RestaurantsService} from '../restaurants/restaurants.service';
+import { Restaurant } from "app/restaurants/restaurant/restaurant.model";
+import {ActivatedRoute}  from "@angular/router";
 
 @Component({
   selector: 'mt-restaurants-details',
@@ -6,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
+
+  restaurant: Restaurant
 
   ngOnInit() {
+    this.restaurantsService.restauranteById(this.route.snapshot.params['id'])
+      .subscribe(restaurant=>this.restaurant = restaurant)
   }
 
 }
